5333f7bafc96a231d26cbe97446dcbfad7caf142
Lars Licha <lars.licha@elektrobit.com>
2025-12-05 16:56:01 +0100

    Default tab settings
    Change-Id: I9a98be0626f4effc5fd12056e931ba8d9e783396

--- init.lua
+++ init.lua
@@ -152,6 +152,11 @@ vim.o.splitbelow = true
 vim.o.list = true
 vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 
+-- Default tab settings that are used when it couldn't be detected
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
+
 -- Preview substitutions live, as you type!
 vim.o.inccommand = 'split'
 
